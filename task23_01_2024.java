00a9b88728bb207198c62676e4a523c759911ee1
KeerthuK29 <163317070+KeerthuK29@users.noreply.github.com>
2024-03-25 10:39:49 +0530

    Add files via upload

---
+++ task23_01_2024.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class first {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+    System.out.println(input);
+    char charArray[] = input.toCharArray();
+    String inputC = sc.nextLine();
+    System.out.println(inputC);
+        switch(inputC){  
+            
+            case "even": 
+                for (int i = 0; i < charArray.length; i++) {
+            if(charArray[i]%2 == 0){
+                
+                System.out.println(charArray[i]);
+            }
+              
+        }
+        System.out.println();
+        break;
+
+            case "odd": 
+            for (int i = 0; i < charArray.length; i++) {
+            if(charArray[i]%2 != 0){
+                
+                System.out.println(charArray[i]);
+            }  
+             
+        }
+        System.out.println();
+        break;
+          default:
+                System.out.println("Invalid input");  
+                return;
+            }  
+        
+        
+    }
+}
+    
