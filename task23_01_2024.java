19da83e09ebcf71397e6039b65052118bad6088a
KeerthuK29 <163317070+KeerthuK29@users.noreply.github.com>
2024-03-25 13:29:28 +0530

    Update task23_01_2024.java

--- task23_01_2024.java
+++ task23_01_2024.java
@@ -1,42 +1,6 @@
-import java.util.Scanner;
-
 public class task23_01_2024 {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String input = sc.nextLine();
-    System.out.println(input);
-    char charArray[] = input.toCharArray();
-    String inputC = sc.nextLine();
-    System.out.println(inputC);
-        switch(inputC){  
-            
-            case "even": 
-                for (int i = 0; i < charArray.length; i++) {
-            if(charArray[i]%2 == 0){
-                
-                System.out.println(charArray[i]);
-            }
-              
-        }
-        System.out.println();
-        break;
-
-            case "odd": 
-            for (int i = 0; i < charArray.length; i++) {
-            if(charArray[i]%2 != 0){
-                
-                System.out.println(charArray[i]);
-            }  
-             
-        }
-        System.out.println();
-        break;
-          default:
-                System.out.println("Invalid input");  
-                return;
-            }  
-        
-        
+public static void main(String[] args) { 
+        System.out.println("Hello World");
     }
 }
     
