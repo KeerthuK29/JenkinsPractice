02bbcc9b623122387ec66c280ea93c3272089848
KeerthuK29 <163317070+KeerthuK29@users.noreply.github.com>
2024-03-27 20:44:41 +0530

    Update task23_01_2024.java

--- task23_01_2024.java
+++ task23_01_2024.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class task23_01_2024 {
 public static void main(String[] args) { 
-        System.out.println("Hello World");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 1st No:");
+        int a=sc.nextInt();
+        System.out.println("Enter the 2nd No:");
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("The Sum is:",+c);
     }
 }
     
