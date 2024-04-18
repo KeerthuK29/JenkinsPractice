0bbd0836bbe54f67226eb0f110583ec03db1c732
KeerthuK29 <163317070+KeerthuK29@users.noreply.github.com>
2024-04-18 13:38:50 +0530

    Update task23_01_2024.java

--- task23_01_2024.java
+++ task23_01_2024.java
@@ -1,8 +1,7 @@
 
 public class task23_01_2024 {
 public static void main(String[] args) { 
-       int a,b;
-         
+       int a=0,b=0;
         int c=a+b;
         System.out.println("The Sum is:"+c);
       
